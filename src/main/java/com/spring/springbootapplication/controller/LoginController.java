e7fa2a1c54cca30ff8145ffe00295e30136db2fc
sato4neko <u.satoshi.0903@gmail.com>
2025-10-22 18:19:09 +0900

    [update] ログイン・ログアウト機能の追加
    ・未登録ユーザーがログイン時にエラーメッセージを表示
    ・ログアウト時にログインページへのリダイレクトするように設定

--- src/main/java/com/spring/springbootapplication/controller/LoginController.java
+++ src/main/java/com/spring/springbootapplication/controller/LoginController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
 
@@ -13,9 +14,9 @@ public class LoginController {
 
     @RequestMapping("/")
     public String index(Model model,Principal principal) {
-        model.addAttribute("title", "Top page");
-        model.addAttribute("msg", "仮のトップページ");
-
+        model.addAttribute("title", "自己紹介");
+        model.addAttribute("profile_detail", "自分の自己紹介文章が入ります。");
+            
         //nullでなければログイン
         if (principal != null) {
             // ログインユーザーの名前を取得
@@ -33,7 +34,7 @@ public class LoginController {
     }
   
     @RequestMapping("/login")
-    public ModelAndView login(ModelAndView mav,
+    public ModelAndView login(RedirectAttributes redirectAttributes,ModelAndView mav,
             @RequestParam(value = "error", required = false) String error) {
         
         //ログイン画面だけボタン非表示の設定
@@ -42,12 +43,12 @@ public class LoginController {
         mav.addObject("loginPage", true); 
         
         mav.setViewName("login");
-        System.out.println(error);
+
+        //ログイン機能の追加： エラーメッセージの表示
         if (error != null) {
-            mav.addObject("msg", "ログインできませんでした。");
-        } else {
-            mav.addObject("msg", "ユーザー名とパスワードを入力：");
+            mav.addObject("errorMessage", "メールアドレス、もしくはパスワードが間違っています。");
         }
+
         return mav;
     }
 }
